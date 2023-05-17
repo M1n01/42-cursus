a6df21384dd3f52b111b37ce9138be384ed883ce
minabe <minabe@student.42tokyo.jp>
2023-05-17 17:31:32 +0900

    first commit

---
+++ includes/philo.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/17 17:22:47 by minabe            #+#    #+#             */
+/*   Updated: 2023/05/17 17:30:43 by minabe           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+# include <stdio.h>
+# include <string.h>
+# include <pthread.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/time.h>
+
+#endif
---
+++ makefile
@@ -0,0 +1,29 @@
+NAME = philo
+
+CC = cc
+CFLAGS = -Wall -Wextra -Werror $(addprefix -I,$(INCDIR))
+
+INCDIR = ./includes
+INC = $(shell find $(INCDIR) -name "*.h" -type f | xargs)
+
+SRCDIR = ./srcs
+SRCS = $(shell find $(SRCDIR) -name "*.c" -type f | xargs)
+OBJS = $(SRCS:%.c=%.o)
+
+all: $(NAME)
+
+$(NAME): $(OBJS)
+		$(CC) $(CFLAGS) $(SRCS) -o $(NAME)
+
+clean:
+		$(RM) $(OBJS) $(B_OBJS)
+
+fclean: clean
+		$(RM) $(NAME)
+
+re: fclean all
+
+debug: CFLAGS += -g -fsanitize=address,leak
+debug: re
+
+.PHONY: all clean fclean re bonus norm debug
