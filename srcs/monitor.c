ba5553b6cd82013f1d9fb5ad836211e1095b3898
Minato Abe <minato.cat.love@gmail.com>
2023-06-28 18:31:39 +0900

    fix: monitor完了

--- srcs/monitor.c
+++ srcs/monitor.c
@@ -6,7 +6,7 @@
 /*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/26 14:25:36 by minabe            #+#    #+#             */
-/*   Updated: 2023/06/28 01:04:35 by minabe           ###   ########.fr       */
+/*   Updated: 2023/06/28 18:25:46 by minabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@ static bool	check_dead(t_philo *philo_data)
 {
 	long long time;
 
+	pthread_mutex_lock(&philo_data->shered.mutex);
 	time = (get_time_diff(philo_data->shered.start_time) - (philo_data->last_eat_time - philo_data->shered.start_time)) / 1000;
+	pthread_mutex_unlock(&philo_data->shered.mutex);
 	if (philo_data->shered.time_to_die < time)
 	{
 		// printf("%lld\n", time);
@@ -35,10 +37,11 @@ void	*monitor(void *d)
 		if (check_dead(philo_data))
 		{
 			print_log(philo_data, "died");
-			pthread_mutex_lock(&philo_data->shered.log);
+			pthread_mutex_lock(&philo_data->shered.mutex);
 			philo_data->is_dead = true;
-			pthread_mutex_unlock(&philo_data->shered.log);
+			pthread_mutex_unlock(&philo_data->shered.mutex);
 			return (NULL);
 		}
+		usleep(100);
 	}
 }
