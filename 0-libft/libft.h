864f5aa264eac6efcda98bd15decb9fd58d2c8b8
M1n01 <abe.minato.bz@gmail.com>
2024-06-08 11:15:41 +0900

    feat: strnlen

--- 0-libft/libft.h
+++ 0-libft/libft.h
@@ -28,6 +28,7 @@ void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t n);
 
 size_t	ft_strlen(const char *str);
+size_t	ft_strnlen(char *str, size_t size);
 size_t	ft_strlcat(char *dst, char *src, size_t dstsize);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 
