e49c2401fa1c664f0fa62f76c7e226b2ca59c0f4
M1n01 <abe.minato.bz@gmail.com>
2024-11-09 18:07:36 +0900

    fix: exam06

--- exam06/mini_serv.c
+++ exam06/mini_serv.c
@@ -1,6 +1,6 @@
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -8,17 +8,17 @@
 typedef struct s_client
 {
 	int id;
-	char msg[1000];
+	char msg[300000];
 } t_client;
 
-t_client clients[1000];
+t_client clients[1024];
 fd_set read_set, write_set, current_set;
 int last_fd = 0, g_id = 0;
-char send_buffer[1000], recv_buffer[1000];
+char send_buffer[300000], recv_buffer[300000];
 
-void error(const char *msg)
+void err(char *msg)
 {
-	if (msg != NULL)
+	if (msg)
 		write(2, msg, strlen(msg));
 	else
 		write(2, "Fatal error", 11);
@@ -26,48 +26,51 @@ void error(const char *msg)
 	exit(1);
 }
 
-void send_to_all(int fd)
+void send_to_all(int except)
 {
-	for (int i = 0; i <= last_fd; i++)
+	for (int fd = 0; fd <= last_fd; fd++)
 	{
-		if (FD_ISSET(i, &write_set) && i != fd)
-			if (send(i, send_buffer, strlen(send_buffer), 0) == -1)
-				error(NULL);
+		if (FD_ISSET(fd, &write_set) && fd != except)
+			if (send(fd, send_buffer, strlen(send_buffer), 0) == -1)
+				err(NULL);
 	}
 }
 
 int main(int argc, char *argv[])
 {
 	if (argc != 2)
-		error("Wrong number of arguments");
+		err("Wrong number of arguments");
+
 	int sd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sd == -1)
-		error(NULL);
+		err(NULL);
 
 	struct sockaddr_in addr;
 	socklen_t len;
 	bzero(&addr, sizeof(addr));
 	addr.sin_family = AF_INET;
 	addr.sin_port = htons(atoi(argv[1]));
-	addr.sin_addr.s_addr = INADDR_ANY;
+	addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
 	FD_ZERO(&current_set);
 	FD_SET(sd, &current_set);
 	bzero(clients, sizeof(clients));
 	last_fd = sd;
 
-	if (bind(sd, (struct sockaddr *)&addr, sizeof(addr)) == -1 || listen(sd, 100) == -1)
-		error(NULL);
+	if (bind(sd, (const struct sockaddr *)&addr, sizeof(addr)) == -1 || listen(sd, 100) == -1)
+		err(NULL);
+
 	while (1)
 	{
-		write_set = read_set = current_set;								  // copy current_set to read_set and write_set
-		if (select(last_fd + 1, &read_set, &write_set, NULL, NULL) == -1) // wait for an event on any of the sockets
+		read_set = write_set = current_set;
+		if (select(last_fd + 1, &read_set, &write_set, NULL, NULL) == -1)
 			continue;
+
 		for (int fd = 0; fd <= last_fd; fd++)
 		{
 			if (FD_ISSET(fd, &read_set))
 			{
-				if (fd == sd) // new client
+				if (fd == sd)
 				{
 					int new_sd = accept(sd, (struct sockaddr *)&addr, &len);
 					if (new_sd == -1)
@@ -82,13 +85,13 @@ int main(int argc, char *argv[])
 				else
 				{
 					int ret = recv(fd, recv_buffer, sizeof(recv_buffer), 0);
-					if (ret <= 0) // client disconnected
+					if (ret <= 0)
 					{
 						sprintf(send_buffer, "server: client %d just left\n", clients[fd].id);
 						send_to_all(fd);
 						FD_CLR(fd, &current_set);
 						close(fd);
-						bzero(clients[fd].msg, sizeof(clients[fd].msg));
+						bzero(clients[fd].msg, strlen(clients[fd].msg));
 					}
 					else
 					{
@@ -100,13 +103,14 @@ int main(int argc, char *argv[])
 								clients[fd].msg[j] = '\0';
 								sprintf(send_buffer, "client %d: %s\n", clients[fd].id, clients[fd].msg);
 								send_to_all(fd);
-								bzero(clients[fd].msg, sizeof(clients[fd].msg));
+								bzero(clients[fd].msg, strlen(clients[fd].msg));
+								j = -1;
 							}
 						}
 					}
 				}
+				break;
 			}
-			break;
 		}
 	}
 	return (0);
