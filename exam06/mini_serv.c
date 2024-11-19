286a9d70f3d7c9d2826b98b76bc214d8c6c244f5
M1n01 <abe.minato.bz@gmail.com>
2024-11-19 10:34:23 +0900

    fix: exam06

--- exam06/mini_serv.c
+++ exam06/mini_serv.c
@@ -12,9 +12,9 @@ typedef struct s_client
 } t_client;
 
 t_client clients[1024];
-fd_set read_set, write_set, current_set;
+fd_set write_set, read_set, current_set;
 int last_fd = 0, g_id = 0;
-char send_buffer[300000], recv_buffer[300000];
+char send_buf[300000], recv_buf[300000];
 
 void error(char *msg)
 {
@@ -30,8 +30,8 @@ void send_to_all(int except)
 {
 	for (int fd = 0; fd <= last_fd; fd++)
 	{
-		if (FD_ISSET(fd, &write_set) && fd != except)
-			if (send(fd, send_buffer, strlen(send_buffer), 0) == -1)
+		if (FD_ISSET(fd, &write_set) && fd != except) // 間違いポイント
+			if (send(fd, send_buf, strlen(send_buf), 0) == -1)
 				error(NULL);
 	}
 }
@@ -39,7 +39,8 @@ void send_to_all(int except)
 int main(int ac, char *av[])
 {
 	if (ac != 2)
-		error("Wrong number of arguments");
+		error("Wrong number of args");
+
 	int sd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sd == -1)
 		error(NULL);
@@ -53,7 +54,7 @@ int main(int ac, char *av[])
 
 	FD_ZERO(&current_set);
 	FD_SET(sd, &current_set);
-	bzero(clients, sizeof(clients));
+	bzero(clients, sizeof(clients)); // 忘れポイント
 	last_fd = sd;
 
 	if (bind(sd, (const struct sockaddr *)&addr, sizeof(addr)) == -1 || listen(sd, 100) == -1)
@@ -62,7 +63,7 @@ int main(int ac, char *av[])
 	while (1)
 	{
 		read_set = write_set = current_set;
-		if (select(last_fd + 1, &read_set, &write_set, NULL, NULL) == -1)
+		if (select(last_fd + 1, &read_set, &write_set, 0, 0) == -1) // 忘れポイント
 			continue;
 
 		for (int fd = 0; fd <= last_fd; fd++)
@@ -77,30 +78,30 @@ int main(int ac, char *av[])
 					if (new_fd > last_fd)
 						last_fd = new_fd;
 					clients[new_fd].id = g_id++;
-					FD_SET(new_fd, &current_set);
-					sprintf(send_buffer, "server: client %d just arrived\n", g_id);
+					FD_SET(new_fd, &current_set); // 忘れポイント
+					sprintf(send_buf, "server: client %d just arrived\n", clients[new_fd].id);
 					send_to_all(new_fd);
 				}
 				else
 				{
-					int ret = recv(fd, recv_buffer, strlen(recv_buffer), 0);
+					int ret = recv(fd, recv_buf, strlen(recv_buf), 0);
 					if (ret <= 0)
 					{
-						sprintf(send_buffer, "server: client %d just left\n", clients[fd].id);
+						sprintf(send_buf, "server: client %d just left\n", clients[fd].id);
 						send_to_all(fd);
+						bzero(clients[fd].msg, strlen(clients[fd].msg));
 						FD_CLR(fd, &current_set);
 						close(fd);
-						bzero(clients[fd].msg, strlen(clients[fd].msg));
 					}
 					else
 					{
 						for (int i = 0, j = strlen(clients[fd].msg); i < ret; i++, j++)
 						{
-							clients[fd].msg[j] = recv_buffer[i];
+							clients[fd].msg[j] = recv_buf[i];
 							if (clients[fd].msg[j] == '\n')
 							{
 								clients[fd].msg[j] = '\0';
-								sprintf(send_buffer, "client %d: %s\n", clients[fd].id, clients[fd].msg);
+								sprintf(send_buf, "client %d: %s\n", clients[fd].id, clients[fd].msg);
 								send_to_all(fd);
 								bzero(clients[fd].msg, strlen(clients[fd].msg));
 								j = -1;
